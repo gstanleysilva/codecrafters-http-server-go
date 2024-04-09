949d944c14251751884e91a081392346bbd0f46e
Gustavo Stanley Silva <guhstanley@gmail.com>
2024-04-08 23:08:48 -0300

    pass stage

--- app/server.go
+++ app/server.go
@@ -4,12 +4,13 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
 var Port = 4221
 
 func main() {
-	fmt.Printf("Server running on port %d", Port)
+	fmt.Printf("Server running on port %d\r\n", Port)
 
 	listener, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%d", Port))
 	if err != nil {
@@ -30,19 +31,67 @@ func main() {
 }
 
 func handleRequests(conn net.Conn) {
-	defer conn.Close()
 
-	var data []byte
-	_, err := conn.Read(data)
-	if err != nil {
-		fmt.Println("failed to read bytes: ", err.Error())
-		os.Exit(1)
+	request := parseRequest(conn)
+
+	fmt.Println(request.Method, request.Path)
+
+	if request.Path == "/" {
+		writeResponse(conn, 200)
+	} else {
+		writeResponse(conn, 404)
 	}
 
-	response := []byte("HTTP/1.1 200 OK \r\n\r\n")
-	_, err = conn.Write(response)
+	conn.Close()
+}
+
+type Request struct {
+	Method  string
+	Path    string
+	Headers map[string]string
+}
+
+func parseRequest(conn net.Conn) Request {
+	var result Request
+
+	buff := make([]byte, 1024)
+
+	size, err := conn.Read(buff)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	strRequest := string(buff[:size])
+
+	lines := strings.Split(strRequest, "\r\n")
+
+	//Start Line
+	parts := strings.Split(lines[0], " ")
+	result.Method = parts[0]
+	result.Path = parts[1]
+
+	return result
+}
+
+func writeResponse(conn net.Conn, statusCode int) {
+	_, err := conn.Write([]byte(getStringMessage(statusCode)))
 	if err != nil {
 		fmt.Println("failed to write bytes: ", err.Error())
 		os.Exit(1)
 	}
+}
+
+func statusToText(statusCode int) string {
+	switch statusCode {
+	case 200:
+		return "OK"
+	case 404:
+		return "Not Found"
+	default:
+		return "Internal Server Error"
+	}
+}
+
+func getStringMessage(statusCode int) string {
+	return fmt.Sprintf("HTTP/1.1 %d %s\r\n\r\n", statusCode, statusToText(statusCode))
 }
