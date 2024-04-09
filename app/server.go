1e797b50f05530bb7edaa72c00638f2e569acda6
Gustavo Stanley Silva <guhstanley@gmail.com>
2024-04-08 21:59:46 -0300

    pass stage

--- app/server.go
+++ app/server.go
@@ -6,18 +6,43 @@ import (
 	"os"
 )
 
-func main() {
-	fmt.Println("Logs from your program will appear here!")
+var Port = 4221
 
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
+func main() {
+	fmt.Printf("Server running on port %d", Port)
+
+	listener, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%d", Port))
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
+	for {
+		// Use for to keep server running
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		go handleRequests(conn)
+	}
+}
+
+func handleRequests(conn net.Conn) {
+	defer conn.Close()
+
+	var data []byte
+	_, err := conn.Read(data)
 	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
+		fmt.Println("failed to read bytes: ", err.Error())
+		os.Exit(1)
+	}
+
+	response := []byte("HTTP/1.1 200 OK \r\n\r\n")
+	_, err = conn.Write(response)
+	if err != nil {
+		fmt.Println("failed to write bytes: ", err.Error())
 		os.Exit(1)
 	}
 }
